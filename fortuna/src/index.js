598d4d94bffb6637cb2b02c2d717f76b6b6f5d78
David Harmeyer <13dharmeyer@gmail.com>
2019-10-10 22:55:08 -0400

    added flow to index.js

--- fortuna/src/index.js
+++ fortuna/src/index.js
@@ -1,15 +1,15 @@
+//@flow strict
+
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
-
-
 
 const styleForRow = {
   'margin-top':'20px',
 };
 
 //This is the Login Page Display
-class LoginPageDisplay extends React.Component{
+class LoginPageDisplay extends React.Component<{||}> {
   
   render() {
     return (
@@ -32,9 +32,13 @@ class LoginPageDisplay extends React.Component{
         </div>
     );
   }
+
 }
 
 
-
 //This is where we render the view for the login page
-ReactDOM.render(<LoginPageDisplay />, document.getElementById('root'));
+const rootComponent=document.getElementById('root');
+if (rootComponent != null) {
+	ReactDOM.render(<LoginPageDisplay />, rootComponent);
+}
+
