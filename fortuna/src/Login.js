4c822fbfb3cbd4e70797b8e9ba1fdb31c08f7747
David Harmeyer <13dharmeyer@gmail.com>
2019-10-11 21:59:04 -0400

    fixed unbalanced html tags

--- fortuna/src/Login.js
+++ fortuna/src/Login.js
@@ -12,23 +12,26 @@ class Login extends React.Component<{||}> {
   render(): React.Node {
     return (
       <div id = "Parent">
-	    <div class = "row">
-	      <h4 class = "col-md-4">LeaderBoard</h4>
-	      <h1 class = "col-md-4 text-center">Fortuna</h1>
-	      <h4 class = "col-md-4 text-right">What is Fortuna?</h4>
-	    <div class = "row" style = {styleForRow}>
-          <div class="col text-center">
-            <button type="button" class="btn btn-success btn-lg">Login</button>
-          </div>
-        </div>
-        <div class = "row" style = {styleForRow}>
-          <div class="col text-center">
-            <button type="button" class="btn btn-secondary btn-sm">Signup</button>
-          </div>
-        </div>
-	  </div>
-	);
+	    	<div class = "row">
+	    	  <h4 class = "col-md-4">LeaderBoard</h4>
+	    	  <h1 class = "col-md-4 text-center">Fortuna</h1>
+	    	  <h4 class = "col-md-4 text-right">What is Fortuna?</h4>
+	    		<div class = "row" style = {styleForRow}>
+      	    <div class="col text-center">
+      	      <button type="button" class="btn btn-success btn-lg">Login</button>
+      	    </div>
+      	  </div>
+      	  <div class = "row" style = {styleForRow}>
+      	    <div class="col text-center">
+      	      <button type="button" class="btn btn-secondary btn-sm">Signup</button>
+      	    </div>
+      	  </div>
+	  		</div>
+			</div>
+		);
   }
 }
+
+
 
 export default Login;
