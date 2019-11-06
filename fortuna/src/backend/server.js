c95253088874a7e8d7229b169cfece49156fde27
Sandbox <edolorfino24@gmail.com>
2019-11-06 14:20:56 -0500

    Updated npm scripts and added DB connection

--- fortuna/src/backend/server.js
+++ fortuna/src/backend/server.js
@@ -4,6 +4,7 @@
 
 // Required Packages/Imports
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Port constant so we may edit as needed
 const PORT = process.env.PORT || 4000;
@@ -12,6 +13,12 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 // As of express v4.16, npm body-parser is bundled with express again
 app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect('mongodb://localhost/fortuna', { useNewUrlParser: true, 
+    useUnifiedTopology: true })
+    .then(() => console.log(`Connected to MongoDB...`))
+    .catch(() => console.error('Could not connect to DB'));
 
 // Server Listener
 app.listen(PORT, () => {
