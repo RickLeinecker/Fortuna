a554ab6b4fc01cdfc34c7f9d45d6987e7a2c61e4
blaiadam <ablair44@live.com>
2019-11-14 16:23:57 -0500

    Fixed indentation

--- fortuna/src/mainmenu/MainMenu.js
+++ fortuna/src/mainmenu/MainMenu.js
@@ -14,38 +14,38 @@ class MainMenu extends React.Component<{||}> {
   render(): React.Node {
     return (
       <div id = "Parent">
-	      <div class = "row">
+        <div class = "row">
           <div class = "col-md-4">
             <Link to="/Login">
               <button type="button" class="btn btn-secondary btn-lg">&lt;- Back to Login</button>
             </Link>
           </div>
-	        <h1 class = "col-md-4 text-center">Main Menu</h1>
-	      </div>
-	      <div class="row" style={styleForRow}>
-	        <div class="col text-center">
+	  <h1 class = "col-md-4 text-center">Main Menu</h1>
+	</div>
+	<div class="row" style={styleForRow}>
+	  <div class="col text-center">
             <button type="button" class="btn btn-primary">Battle Arena</button>
           </div>
         </div>
         <div class="row" style={styleForRow}>
-	        <div class="col text-center">
+	  <div class="col text-center">
             <button type="button" class="btn btn-info">Training Arena</button>
           </div>
         </div>
         <div class="row" style={styleForRow}>
-	        <div class="col text-center">
+	  <div class="col text-center">
             <Link to="/Marketplace">
               <button type="button" class="btn btn-info">Marketplace</button>
             </Link>
           </div>
         </div>
         <div class="row" style={styleForRow}>
-	        <div class="col text-center">
+	  <div class="col text-center">
             <button type="button" class="btn btn-info">Armory</button>
           </div>
         </div>
       </div>
-	  );
+    );
   }
 }
 
