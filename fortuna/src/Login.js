238f8aa8ee18ab8303d68e5f37d8bd73a175559d
Zexdur <Zexdur@gmail.com>
2019-12-12 15:30:12 -0500

    This is a redesign of Login
    I have redesigned login and have started the api hookup. I want to merge it to make sure it will work with master

--- fortuna/src/Login.js
+++ fortuna/src/Login.js
@@ -1,13 +1,13 @@
 //@flow strict
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ReactDOM from 'react-dom';
-import MarketPlace from './MarketPlace.js';
+import LoginPopup from './LoginPopup.js';
 import './Login.css';
-const PORT = 4000;
+const PORT = 3000;
 
   //This is the Login Page Display
-  class LoginPageDisplay extends React.Component {
+  class Login extends React.Component<{||}> {
     state = {
       response: '',
       userName: '',
@@ -19,10 +19,10 @@ const PORT = 4000;
       this.callApi()
         .then(res => this.setState({ response: res.express }))
         .catch(err => console.log(err));
-    }
+    };
     
     callApi = async () => {
-      const response = await fetch('http://localhost:'+PORT+'/backend/routes/api/signup');
+      const response = await fetch('http://localhost:'+PORT+'/backend/routes/api/signup.js');
       const body = await response.json();
       if (response.status !== 200) throw Error(body.message);
       
@@ -31,8 +31,7 @@ const PORT = 4000;
     
     handleLoginClick = async e => {
       e.preventDefault();
-      LoginFunction();
-      const response = await fetch('http://localhost:'+PORT+'/backend/routes/api/signup', {
+      const response = await fetch('http://localhost:'+PORT+'/backend/routes/api/signup.js', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -43,7 +42,7 @@ const PORT = 4000;
       console.log(body);
     };
 
-    render(){
+    render():React.Node{
       return (
         <div id = "Parent">
             <div className = "row">
@@ -52,41 +51,58 @@ const PORT = 4000;
                 <h4 className = "col-md-4 text-right">What is Fortuna?</h4>
             </div>
             <div className = "row styleForRow">
-              <div className="col text-center">
-                  <input
-                    type="text"
-                    value={this.state.userName}
-                    onChange={e => this.setState({ userName: e.target.value })}
-                  />
-                  <input
-                    type="text"
-                    value={this.state.password}
-                    onChange={e => this.setState({ password: e.target.value })}
-                  />
-                  <button type="button" className="btn btn-success btn-lg" onClick={this.handleLoginClick}>Login</button>
+              <div className="container h-100">
+                <div className="row h-100 justify-content-center align-items-center">
+                  <div className="card">
+                    <h4 className="card-header">Login</h4>
+                    <div className="card-body">
+                      <form data-toggle="validator" role="form" method="post" action="#">
+                        <div className="row">
+                          <div className="col-md-12">
+                            <div className="form-group">
+                              <label>UserName</label>
+                              <div className="input-group">
+                                <input type="text" className="form-control" name="loginUserName" value={this.state.userName} onChange={e => this.setState({ userName: e.target.value })} />
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="col-md-12">
+                            <div className="form-group">
+                              <label>Password</label>
+                              <div className="input-group">
+                                <input type="password" name="loginPassword" className="form-control"/>
+                              </div>
+                                <div className="help-block with-errors text-danger"></div>
+                              </div>
+                            </div>
+                          </div>
+                            <div className="row">
+                              <div className="col-md-12">
+                              <Link to="/MainMenu">
+                                  <button type="button" className="btn btn-primary btn-lg btn-block" onClick={this.handleLoginClick}>Login</button>
+                              </Link>
+                              </div>
+                            </div>
+                        </form>
+                        <div className = "row styleForRow">
+                          <div className="col text-center">
+                          <button type="button" className="btn btn-secondary btn-sm">Signup</button>
+                          </div>
+                        </div>
+                       </div>
+                     </div>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className = "row styleForRow">
-              <div className="col text-center">
-                  <button type="button" className="btn btn-secondary btn-sm">Signup</button>
-              </div>
-            </div>
+            
           </div>
       );
     }
   
   }
 
-  //This is the function that gets used when the player clicks login on the dashboard
-  function LoginFunction()
-  {
-    const rootComponent=document.getElementById('root');
-    if (rootComponent != null) {
-        ReactDOM.render(<MarketPlace/>, rootComponent);
-    }
-  }
+  export default Login;
 
-  export default LoginPageDisplay;
 
-  
-  
