3d0fa6f5633e0e42748d508e4995c0985ee0ad2e
Zexdur <Zexdur@gmail.com>
2019-12-22 16:17:22 -0500

    Passes Flow and Passes my tests

--- fortuna/src/backend/server.js
+++ fortuna/src/backend/server.js
@@ -7,16 +7,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 // Port constant so we may edit as needed
-const PORT = process.env.PORT || 4000;
+const PORT = 3001;
 
 const signups = require('./routes/api/signup');
 const home = require('./routes/api/home');
-
+const cors = require('cors');
 // Server Instance
 const app = express();
 // As of express v4.16, npm body-parser is bundled with express again
 app.use(express.json());
-
+app.use(cors());
 // Use the api routes as middleware
 // First param is the url?uri  directory second is the api path
 app.use('/', home);
