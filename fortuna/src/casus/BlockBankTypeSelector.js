f7c7ece696e5330050585432db7d465c410acf35
David Harmeyer <13dharmeyer@gmail.com>
2019-12-26 21:36:50 -0600

    fixed indentation in login

--- fortuna/src/casus/BlockBankTypeSelector.js
+++ fortuna/src/casus/BlockBankTypeSelector.js
@@ -1,6 +1,7 @@
 //@flow strict
 
 import * as React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class BlockBankTypeSelector extends React.Component<{||}> {
 
@@ -12,6 +13,10 @@ class BlockBankTypeSelector extends React.Component<{||}> {
 		}
 		return (
 			<div style={style}>
+				<button className="button">Tab 1</button>
+				<button>Tab 2</button>
+				<button>Tab 3</button>
+				<button>Tab 4</button>
 			</div>
 		);
 	}
