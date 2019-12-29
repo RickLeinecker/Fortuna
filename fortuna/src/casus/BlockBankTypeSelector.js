1085be0d30ebb43c471963bbd32194d3b64ca704
David Harmeyer <13dharmeyer@gmail.com>
2019-12-29 15:52:52 -0600

    working on BlockBankTypeSelector, but saving changes to review other peoples code

--- fortuna/src/casus/BlockBankTypeSelector.js
+++ fortuna/src/casus/BlockBankTypeSelector.js
@@ -2,8 +2,15 @@
 
 import * as React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './BlockBankTypeSelector.css';
+
+import type {BlockBankType} from './BlockBankType.js';
 
 class BlockBankTypeSelector extends React.Component<{||}> {
+
+	showSection(section: BlockBankType):void {
+		console.log('showing section '+section);
+	}
 
 	render():React.Node {
 		const style = {
@@ -11,12 +18,40 @@ class BlockBankTypeSelector extends React.Component<{||}> {
 			height: '100%',
 			width: '200px'
 		}
+
+		//sections:
+		//	- Control Flow (if / if-else / loops)
+		//	- Variables (Set __ to / Get variables)
+		//	- Math (angleTo, abs, +, -, *, /)
+		//	- Logic (and / or / xor)
+		//	- Lists
 		return (
 			<div style={style}>
-				<button className="button">Tab 1</button>
-				<button>Tab 2</button>
-				<button>Tab 3</button>
-				<button>Tab 4</button>
+				<button 
+					className="btn btn-secondary paddedButton" 
+					onClick={() => this.showSection('CONTROL_FLOW')}>
+						Control Flow
+				</button>
+				<button 
+					className="btn btn-secondary paddedButton" 
+					onClick={() => this.showSection('VARIABLES')}>
+						Variables
+				</button>
+				<button 
+					className="btn btn-secondary paddedButton" 
+					onClick={() => this.showSection('MATH')}>
+						Math
+				</button>
+				<button 
+					className="btn btn-secondary paddedButton" 
+					onClick={() => this.showSection('LOGIC')}>
+						Logic
+				</button>
+				<button 
+					className="btn btn-secondary paddedButton" 
+					onClick={() => this.showSection('LISTS')}>
+						Lists
+				</button>
 			</div>
 		);
 	}
