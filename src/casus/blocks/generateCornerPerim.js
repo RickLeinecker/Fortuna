fe8925bd345fd09e6f9df6e3e71951fe9656cda0
David Harmeyer <13dharmeyer@gmail.com>
2020-01-27 10:37:28 -0500

    Added code to generate list perimeters

--- src/casus/blocks/generateCornerPerim.js
+++ src/casus/blocks/generateCornerPerim.js
@@ -82,6 +82,60 @@ function _generateDoublePerim(boundingBox: BoundingBox): Array<Vec> {
 	return perim;
 }
 
+function _generateIntListPerim(boundingBox: BoundingBox): Array<Vec> {
+	const perim: Array<Vec> = [];
+	perim.push(new Vec(boundingBox.x+boundingBox.w, boundingBox.y));
+	perim.push(new Vec(boundingBox.x+boundingBox.w-RAMP_WIDTH, boundingBox.y+boundingBox.h/2));
+	perim.push(new Vec(boundingBox.x+boundingBox.w, boundingBox.y+boundingBox.h/2));
+	perim.push(new Vec(boundingBox.x+boundingBox.w-RAMP_WIDTH, boundingBox.y+boundingBox.h));
+
+	for (let i:number = -N_POINTS_TO_APROX_CIRCLE; i<=N_POINTS_TO_APROX_CIRCLE; i++) {
+		const centerX = boundingBox.x+RAMP_WIDTH;
+		const centerY = boundingBox.y+boundingBox.h/2;
+		const angle = Math.PI/2 * i/N_POINTS_TO_APROX_CIRCLE + Math.PI;
+		const xOffset = RAMP_WIDTH*Math.cos(angle);
+		const yOffset = boundingBox.h/2*Math.sin(angle);
+		
+		perim.push(new Vec(centerX + xOffset, centerY + yOffset));
+	}
+	return perim;
+}
+
+function _generateDoubleListPerim(boundingBox: BoundingBox): Array<Vec> {
+	const perim: Array<Vec> = [];
+	perim.push(new Vec(boundingBox.x+boundingBox.w, boundingBox.y));
+	perim.push(new Vec(boundingBox.x+boundingBox.w-RAMP_WIDTH, boundingBox.y+boundingBox.h/2));
+	perim.push(new Vec(boundingBox.x+boundingBox.w, boundingBox.y+boundingBox.h/2));
+	perim.push(new Vec(boundingBox.x+boundingBox.w-RAMP_WIDTH, boundingBox.y+boundingBox.h));
+
+	perim.push(new Vec(boundingBox.x, boundingBox.y+boundingBox.h));
+	for (let i:number = 0; i<=N_POINTS_TO_APROX_CIRCLE; i++) {
+		const centerX = boundingBox.x+RAMP_WIDTH;
+		const centerY = boundingBox.y+boundingBox.h/2;
+		const angle = Math.PI/2 * i/N_POINTS_TO_APROX_CIRCLE + Math.PI;
+		const xOffset = RAMP_WIDTH*Math.cos(angle);
+		const yOffset = boundingBox.h/2*Math.sin(angle);
+		
+		perim.push(new Vec(centerX + xOffset, centerY + yOffset));
+	}
+
+	return perim;
+}
+
+function _generateBooleanListPerim(boundingBox: BoundingBox): Array<Vec> {
+	const perim: Array<Vec> = [];
+	perim.push(new Vec(boundingBox.x+boundingBox.w, boundingBox.y));
+	perim.push(new Vec(boundingBox.x+boundingBox.w-RAMP_WIDTH, boundingBox.y+boundingBox.h/2));
+	perim.push(new Vec(boundingBox.x+boundingBox.w, boundingBox.y+boundingBox.h/2));
+	perim.push(new Vec(boundingBox.x+boundingBox.w-RAMP_WIDTH, boundingBox.y+boundingBox.h));
+
+	perim.push(new Vec(boundingBox.x + RAMP_WIDTH, boundingBox.y + boundingBox.h));
+	perim.push(new Vec(boundingBox.x, boundingBox.y + boundingBox.h/2));
+	perim.push(new Vec(boundingBox.x + RAMP_WIDTH, boundingBox.y));
+
+	return perim;
+}
+
 function _generateVoidPerim(boundingBox: BoundingBox): Array<Vec> {
 	const perim: Array<Vec> = [];
 	perim.push(new Vec(boundingBox.x, boundingBox.y));
@@ -95,6 +149,9 @@ function generateCornerPerim(boundingBox: BoundingBox, dataType: DataType): Arra
 	if (dataType==='BOOLEAN') return _generateBoolPerim(boundingBox);
 	if (dataType==='INT') return _generateIntPerim(boundingBox);
 	if (dataType==='DOUBLE') return _generateDoublePerim(boundingBox);
+	if (dataType==='INT_LIST') return _generateIntListPerim(boundingBox);
+	if (dataType==='DOUBLE_LIST') return _generateDoubleListPerim(boundingBox);
+	if (dataType==='BOOLEAN_LIST') return _generateBooleanListPerim(boundingBox);
 	if (dataType==='VOID') return _generateVoidPerim(boundingBox);
 	return [];
 }
