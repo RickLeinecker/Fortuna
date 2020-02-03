9810b8920bded3304cfc6ce9a0e67badde4368d5
Zexdur <Zexdur@gmail.com>
2020-02-02 22:14:26 -0500

    New Marketplace and Buttons
    Updated Marketplace and made it up to par with review. New Buttons are also in the main css

--- src/marketplace/Marketplace.js
+++ src/marketplace/Marketplace.js
@@ -3,11 +3,61 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../Main.css';
+import ChassisView from './ChassisView.js';
+import CannonsViews from './CannonsView.js';
+import CasusBlocksView from './CasusBlocksView.js';
+import JammersView from './JammersView.js';
+import TreadsView from './TreadsView.js';
+import AccessoriesView from './AccessoriesView.js';
+import ScannersView from './ScannersView.js';
+import MarketPlaceViewDataTypes from './MarketPlaceViewDataTypes.js';
+
+type Props = {||}; 
+type State = {|
+	marketplaceView: MarketPlaceViewDataTypes,
+|};
+
 
 // Marketplace component.
-class Marketplace extends React.Component<{||}> {
+class Marketplace extends React.Component<Props, State> {
+
+	constructor(){
+		super();
+		this.state={
+			marketplaceViewClicked: 'NONE'
+		}
+	}
 
   	render(){
+		let partView='NONE';
+		switch(this.state.marketplaceViewClicked) {
+			case 'CHASSIS':
+				console.log("Here");
+				partView=(<ChassisView></ChassisView>);
+				break;
+			case 'CANNONS':
+				partView=(<CannonsViews></CannonsViews>);
+				break;
+			case 'SCANNERS':
+				partView=(<ScannersView></ScannersView>);
+				break;
+			case 'JAMMERS':
+				partView=(<JammersView></JammersView>);
+				break;
+			case 'TREADS':
+				partView=(<TreadsView></TreadsView>);
+				break;
+			case 'ACCESSORIES':
+				partView=(<AccessoriesView></AccessoriesView>);
+				break;
+			case 'CASUS_BLOCKS':
+				partView=(<CasusBlocksView></CasusBlocksView>);
+				break;
+			case 'NONE':
+				partView=(<h2>Select a type on the left</h2>);
+				break;
+			}
 		return (
 	  		<div id="Parent">
 				<div className="row">
@@ -21,31 +71,27 @@ class Marketplace extends React.Component<{||}> {
 				</div>
 				<div className="row mt-5">
 		  			<div className="col-md-4">
-						<h2>Available Tanks</h2>
-						<h6>Tank One</h6>
-						<h6>Tank Two</h6>
-						<h6>Tank Three</h6>
-						<h6>Tank Four</h6>
-						<h6>Tank Five</h6>
-						<h6>Tank Six</h6>
-						<h6>Tank Seven</h6>
-						<h6>Tank Eight</h6>
+					  <div className="list-group col-md-6">
+						<a className="list-group-item list-group-item-action disabled">Category</a>
+						<a className="list-group-item list-group-item-action" onClick={() => this.setState({marketplaceViewClicked:'CANNONS'})}>Cannons</a>
+						<a className="list-group-item list-group-item-action" onClick={() => this.setState({marketplaceViewClicked:'SCANNERS'})}>Scanners</a>
+						<a className="list-group-item list-group-item-action" onClick={() => this.setState({marketplaceViewClicked:'CHASSIS'})}>Chassis</a>
+						<a className="list-group-item list-group-item-action" onClick={() => this.setState({marketplaceViewClicked:'JAMMERS'})}>Jammers</a>
+						<a className="list-group-item list-group-item-action" onClick={() => this.setState({marketplaceViewClicked:'TREADS'})}>Treads</a>
+						<a className="list-group-item list-group-item-action" onClick={() => this.setState({marketplaceViewClicked:'ACCESSORIES'})}>Accessories</a>
+						<a className="list-group-item list-group-item-action" onClick={() => this.setState({marketplaceViewClicked:'CASUS_BLOCKS'})}>Casus Blocks</a>
+					  </div>
 		  			</div>
 		  			<div className="col-md-4">
-						<h2 className="text-center">Selected Tank</h2>
-		  			</div>
-		  			<div className="col-md-4">
-						<h2 className="text-right">Components</h2>
-						<h6 className="text-right">Tank One Cannon</h6>
-						<h6 className="text-right">Tank One Tracks</h6>
-						<h6 className="text-right">Tank One Scanner</h6>
-						<h6 className="text-right">Tank One Jammer</h6>
-						<h6 className="text-right">Tank One Base</h6>
+						<div className="text-center">
+						{partView}
+						</div>
 		  			</div>
 				</div>
 	  		</div>
 		);
-  	}
+	  }
+	  
 }
 
 export default Marketplace;
