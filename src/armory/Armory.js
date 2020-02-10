96b6afcf3759459c58296ba2cf62db80201998ce
blaiadam <ablair44@live.com>
2020-02-10 18:50:30 -0500

    Fixed indentation

--- src/armory/Armory.js
+++ src/armory/Armory.js
@@ -14,11 +14,11 @@ class Armory extends React.Component<{||}> {
 				<div className="column armoryleft">
 					<h3>Select a Tank to Edit</h3>
 					<select className="dropdownMenu">
-                    	<option defaultValue>Select a Tank</option>
-                    	<option value="Child Consumer">Child Consumer</option>
-                    	<option value="Fast Bang">Fast Bang</option>
-                    	<option value="Biggest Gun">Biggest Gun</option>
-                	</select>
+                    				<option defaultValue>Select a Tank</option>
+                    				<option value="Child Consumer">Child Consumer</option>
+                    				<option value="Fast Bang">Fast Bang</option>
+                    				<option value="Biggest Gun">Biggest Gun</option>
+                			</select>
 					<h6>Set this tank as default?</h6>
 					<button type="button" className="btn">Set Default</button>
 					<h3>Edit tank's Code</h3>
@@ -33,74 +33,74 @@ class Armory extends React.Component<{||}> {
 				<div className="column armoryright">
 					<h6>Chassis</h6>
 					<select className="tankComponentMenu">
-                    	<option defaultValue></option>
-                    	<option value="moddableLight">Moddable Light</option>
-                    	<option value="light">Light</option>
-                    	<option value="heavy">Heavy</option>
-                	</select>
+                    				<option defaultValue></option>
+                    				<option value="moddableLight">Moddable Light</option>
+                    				<option value="light">Light</option>
+                    				<option value="heavy">Heavy</option>
+                			</select>
 					<h6>Weapons</h6>
 					<select className="tankComponentMenu">
-                    	<option defaultValue></option>
-                    	<option value="1">Laser</option>
-                    	<option value="2">Machine Gun</option>
-                    	<option value="3">Vulcan Cannon</option>
-                	</select>
+                    				<option defaultValue></option>
+                    				<option value="1">Laser</option>
+                    				<option value="2">Machine Gun</option>
+                    				<option value="3">Vulcan Cannon</option>
+                			</select>
 					<select className="tankComponentMenu">
-                    	<option defaultValue></option>
-                    	<option value="1">Laser</option>
-                    	<option value="2">Machine Gun</option>
-                    	<option value="3">Vulcan Cannon</option>
-                	</select>
+						<option defaultValue></option>
+						<option value="1">Laser</option>
+						<option value="2">Machine Gun</option>
+						<option value="3">Vulcan Cannon</option>
+					</select>
 					<h6>Scanner</h6>
 					<select className="tankComponentMenu">
-                    	<option defaultValue></option>
-                    	<option value="1">Short Range Scanner</option>
-                    	<option value="2">Medium Range Scanner</option>
-                    	<option value="3">Long Range Scanner</option>
-                	</select>
+						<option defaultValue></option>
+						<option value="1">Short Range Scanner</option>
+						<option value="2">Medium Range Scanner</option>
+						<option value="3">Long Range Scanner</option>
+					</select>
 					<select className="tankComponentMenu">
-                    	<option defaultValue></option>
-                    	<option value="1">Anti-Jammer Scanner</option>
-                    	<option value="2">Item Scanner</option>
-                	</select>
+						<option defaultValue></option>
+						<option value="1">Anti-Jammer Scanner</option>
+						<option value="2">Item Scanner</option>
+					</select>
 					<select className="tankComponentMenu">
-                    	<option defaultValue></option>
-                    	<option value="1">Anti-Jammer Scanner</option>
-                    	<option value="2">Item Scanner</option>
-                	</select>
+						<option defaultValue></option>
+						<option value="1">Anti-Jammer Scanner</option>
+						<option value="2">Item Scanner</option>
+					</select>
 					<h6>Jammer</h6>
 					<select className="tankComponentMenu">
-                    	<option defaultValue></option>
-                    	<option value="1">Short Range Jammer</option>
-                    	<option value="2">Medium Range Jammer</option>
-                    	<option value="3">Long Range Jammer</option>
-                	</select>
+						<option defaultValue></option>
+						<option value="1">Short Range Jammer</option>
+						<option value="2">Medium Range Jammer</option>
+						<option value="3">Long Range Jammer</option>
+					</select>
 					<h6>Treads</h6>
 					<select className="tankComponentMenu">
-                    	<option defaultValue></option>
-                    	<option value="1">Advanced Treads</option>
-                    	<option value="2">Armored Treads</option>
-                    	<option value="3">Fast Treads</option>
-                	</select>
+						<option defaultValue></option>
+						<option value="1">Advanced Treads</option>
+						<option value="2">Armored Treads</option>
+						<option value="3">Fast Treads</option>
+					</select>
 					<h6>Single-Use Items</h6>
 					<select className="tankComponentMenu">
-                    	<option defaultValue></option>
-                    	<option value="1">Missile Tracking Beacon</option>
-                    	<option value="2">C4</option>
-                    	<option value="3">Mine</option>
-                	</select>
+						<option defaultValue></option>
+						<option value="1">Missile Tracking Beacon</option>
+						<option value="2">C4</option>
+						<option value="3">Mine</option>
+					</select>
 					<select className="tankComponentMenu">
-                    	<option defaultValue></option>
-                    	<option value="1">Missile Tracking Beacon</option>
-                    	<option value="2">C4</option>
-                    	<option value="3">Mine</option>
-                	</select>
+						<option defaultValue></option>
+						<option value="1">Missile Tracking Beacon</option>
+						<option value="2">C4</option>
+						<option value="3">Mine</option>
+					</select>
 					<select className="tankComponentMenu">
-                    	<option defaultValue></option>
-                    	<option value="1">Missile Tracking Beacon</option>
-                    	<option value="2">C4</option>
-                    	<option value="3">Mine</option>
-                	</select>
+						<option defaultValue></option>
+						<option value="1">Missile Tracking Beacon</option>
+						<option value="2">C4</option>
+						<option value="3">Mine</option>
+					</select>
 				</div>
 			</div>
 		);
