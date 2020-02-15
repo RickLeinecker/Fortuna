0493a601623a40d0d01aa1a05f08f71a7fb49771
Derplime <edolorfino24@gmail.com>
2020-02-15 17:58:44 -0500

    Initial take on getMarketSales

--- src/backend/controllers/marketControllers.js
+++ src/backend/controllers/marketControllers.js
@@ -11,3 +11,13 @@ import type {
 // MarketplaceSale Model
 import MarketSale from '../../models/marketSaleModel';
 
+exports.getMarketSales = async (req: $Request, res: $Response) => {
+    try {
+        const salesList = await MarketSale.find();
+        res.json(salesList);
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).json({ msg: 'Unable to find list of sales' });
+    }
+}
