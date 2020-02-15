db084aa036c2e33c8f796f70ae983f994d0bbf3e
Derplime <edolorfino24@gmail.com>
2020-02-15 15:02:14 -0500

    Created marketControllers.js and marketRoutes.js

---
+++ src/backend/controllers/marketControllers.js
@@ -0,0 +1,13 @@
+// @flow strict
+
+// Package Imports
+import type {
+    $Request,
+    $Response,
+    NextFunction,
+    Middleware
+} from 'express';
+
+// MarketplaceSale Model
+import MarketSale from '../../models/marketSaleModel';
+
