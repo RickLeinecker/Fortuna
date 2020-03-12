fc83fb1c433e271f2741b3db3f1d96c6393e4357
Zexdur <Zexdur@gmail.com>
2020-03-12 17:22:37 -0400

    Update Armory.js

--- src/armory/Armory.js
+++ src/armory/Armory.js
@@ -114,8 +114,7 @@ class Armory extends React.Component<Props, State> {
 				'x-auth-token': token
 			},
 		});
-		const body = await response.text();
-		const jsonObjectOfTanks = JSON.parse(body);
+		const jsonObjectOfTanks = await response.json();
 		//Clear the data so that we dont duplicate items
 		this.clearInventoryArrays();
 		//This will get the seleced tanks info and fill out the selected items
@@ -124,20 +123,80 @@ class Armory extends React.Component<Props, State> {
 			obj['value'] = jsonObjectOfTanks[tank]._id;
 			obj['label'] = jsonObjectOfTanks[tank].tankName;
 			if(jsonObjectOfTanks[tank]._id === this.state.selectedTankId) {
+				//The following may seem like a lot so here is the gist of it
+				//This is only ran for the selected tank of the user
+				//It will set each component of the selected for the tank 
+				//If that component is null, then it will set it to empty as javascript gives errors for null being a value in a select
 				this.setState({selectedTankName:jsonObjectOfTanks[tank].tankName});
+				if(jsonObjectOfTanks[tank].tankName === null)
+				{
+					this.setState({selectedTankName:""});
+				}
 				this.setState({selectedCasusCode:jsonObjectOfTanks[tank].casusCode});
+				if(jsonObjectOfTanks[tank].casusCode === null)
+				{
+					this.setState({selectedCasusCode:""});
+				}
 				this.setState({selectedIsBot:jsonObjectOfTanks[tank].isBot});
+				if(jsonObjectOfTanks[tank].isBot === null)
+				{
+					this.setState({selectedIsBot:""});
+				}
 				this.setState({selectedChassis: jsonObjectOfTanks[tank].components[0]});
+				if(jsonObjectOfTanks[tank].components[0] === null)
+				{
+					this.setState({selectedChassis:""});
+				}
 				this.setState({selectedWeaponOne: jsonObjectOfTanks[tank].components[1]});
+				if(jsonObjectOfTanks[tank].components[1] === null)
+				{
+					this.setState({selectedWeaponOne:""});
+				}
 				this.setState({selectedWeaponTwo: jsonObjectOfTanks[tank].components[2]});
+				if(jsonObjectOfTanks[tank].components[2] === null)
+				{
+					this.setState({selectedWeaponTwo:""});
+				}
 				this.setState({selectedScannerOne: jsonObjectOfTanks[tank].components[3]});
+				if(jsonObjectOfTanks[tank].components[3] === null)
+				{
+					this.setState({selectedScannerOne:""});
+				}
 				this.setState({selectedScannerTwo: jsonObjectOfTanks[tank].components[4]});
+				if(jsonObjectOfTanks[tank].components[4] === null)
+				{
+					this.setState({selectedScannerTwo:""});
+				}
 				this.setState({selectedScannerThree: jsonObjectOfTanks[tank].components[5]});
+				if(jsonObjectOfTanks[tank].components[5] === null)
+				{
+					this.setState({selectedScannerThree:""});
+				}
 				this.setState({selectedJammer: jsonObjectOfTanks[tank].components[6]});
+				if(jsonObjectOfTanks[tank].components[6] === null)
+				{
+					this.setState({selectedJammer:""});
+				}
 				this.setState({selectedThreads: jsonObjectOfTanks[tank].components[7]});
+				if(jsonObjectOfTanks[tank].components[7] === null)
+				{
+					this.setState({selectedThreads:""});
+				}
 				this.setState({selectedSingleUseItemOne: jsonObjectOfTanks[tank].components[8]});
+				if(jsonObjectOfTanks[tank].components[8] === null)
+				{
+					this.setState({selectedSingleUseItemOne:""});
+				}
 				this.setState({selectedSingleUseItemTwo: jsonObjectOfTanks[tank].components[9]});
+				if(jsonObjectOfTanks[tank].components[9] === null)
+				{
+					this.setState({selectedSingleUseItemTwo:""});
+				}
 				this.setState({selectedSingleUseItemThree: jsonObjectOfTanks[tank].components[10]});
+				if(jsonObjectOfTanks[tank].components[10] === null)
+				{
+					this.setState({selectedSingleUseItemThree:""});
+				}
 			}
 			tankOptions.push(obj);
 		}
