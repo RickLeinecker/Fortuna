43c1e1e829455117097c2fc45c82d0918962a637
David Harmeyer <13dharmeyer@gmail.com>
2020-03-15 20:10:49 -0400

    Made smooth gun rotation based on Casus input

--- src/tanks/Gun.js
+++ src/tanks/Gun.js
@@ -5,10 +5,46 @@ import {getImage} from '../battleground/ImageLoader.js';
 import ImageDrawer from '../battleground/ImageDrawer.js';
 import Vec from '../casus/blocks/Vec.js';
 
+const LERP_PERCENT=0.5;
+const GUN_CENTER_TO_TANK_CENTER=2;
+const GUN_CENTER_TO_GUN_ROT=2;
 class Gun extends TankPart {
 
-	drawSelf(drawer: ImageDrawer, parentPos: Vec, parentRotation: number) {
-		drawer.draw(getImage('RED_GUN_1'), parentPos, 15, parentRotation-Math.PI/2);
+	//in order for the gun movement to look smooth but still be easy to write code for,
+	//the gun will actually turn instantly, but it will look like it turns more slowly.
+	//If you fire a bullet, the bullet will go in the gunAngle. However, the actual gun
+	//is rendered at the displayAngle
+	gunAngle: float;
+	displayAngle: float;
+
+	constructor() {
+		super();
+		this.gunAngle=0;
+		this.displayAngle=0;
+	}
+
+	setTargetGunAngle(gunAngle: float): void {
+		this.gunAngle=gunAngle;
+	}
+
+	onUpdate(): void {
+		//lerp the displayAngle towards the gun angle
+		//lerp is short for linear interpolation
+		const TAU=2*Math.PI;
+		const positiveDistance=((this.gunAngle-this.displayAngle)%TAU+TAU)%TAU;
+		const negativeDistance=((this.displayAngle-this.gunAngle)%TAU+TAU)%TAU;
+		if (positiveDistance<negativeDistance) {
+			this.displayAngle+=positiveDistance*LERP_PERCENT;
+		}
+		else {
+			this.displayAngle-=negativeDistance*LERP_PERCENT;
+		}
+	}
+
+	drawSelf(drawer: ImageDrawer, parentPos: Vec, parentRotation: number): void {
+		const gunRotAround=parentPos.add(new Vec(-GUN_CENTER_TO_TANK_CENTER, 0).rotate(parentRotation));
+		const imageCenter=gunRotAround.add(new Vec(GUN_CENTER_TO_GUN_ROT, 0).rotate(this.displayAngle));
+		drawer.draw(getImage('RED_GUN_1'), imageCenter, 15, this.displayAngle-Math.PI/2);
 	}
 
 }
