da00738ed0d210147d6f0996ec08f666a7fb0de3
David Harmeyer <13dharmeyer@gmail.com>
2020-03-20 13:46:19 -0400

    removed all new circular dependencies created when merging with Master

--- src/tanks/TankPart.js
+++ src/tanks/TankPart.js
@@ -1,10 +1,10 @@
 //@flow strict
 
-import ImageDrawer from '../battleground/ImageDrawer.js';
-import Vec from '../casus/blocks/Vec.js';
-import Battleground from '../battleground/Battleground.js';
-import Tank from './Tank.js';
-import InterpriterState from '../casus/interpreter/InterpriterState.js';
+import type ImageDrawer from '../battleground/ImageDrawer.js';
+import type Vec from '../casus/blocks/Vec.js';
+import type Battleground from '../battleground/Battleground.js';
+import type Tank from './Tank.js';
+import type InterpriterState from '../casus/interpreter/InterpriterState.js';
 
 //A parent class for all tank parts
 
