21d61bd7e3ca9a809c6e8a1624bd6f181cb78928
David Harmeyer <13dharmeyer@gmail.com>
2020-03-20 22:30:07 -0400

    Adjusted tank stats based on their chassis and treads

--- src/tanks/Treads.js
+++ src/tanks/Treads.js
@@ -5,6 +5,21 @@ import {getImage} from '../battleground/ImageLoader.js';
 import ImageDrawer from '../battleground/ImageDrawer.js';
 import Vec from '../casus/blocks/Vec.js';
 
+// 4 Tread types:
+// 
+// Tread 1: Heavily Armored Treads
+//   -2 linear speed, -1 rot speed, +2 armor, +2 point cost
+//
+// Tread 2: Advanced Treads
+//   +2 linear speed, +1 rot speed, +0 armor, +2 point cost
+//	
+// Tread 3: Fast Treads
+//   +1 linear speed, +1 rot speed, +0 armor, +1 point cost
+//
+// Tread 4: Armored Treads
+//   -1 linear speed, -1 rot speed, +1 armor, +1 point cost
+//
+
 type TreadType = 
 	'TREAD_1' |
 	'TREAD_2' |
@@ -21,6 +36,62 @@ class Treads extends TankPart {
 
 	drawSelf(drawer: ImageDrawer, parentPos: Vec, parentRotation: number) {
 		drawer.draw(getImage(this.treadType), parentPos, 10, parentRotation-Math.PI/2);
+	}
+
+	getMoveSpeedMultiplier(): number {
+		switch (this.treadType) {
+			case 'TREAD_1':
+				return 0.80;
+			case 'TREAD_2':
+				return 1.2;
+			case 'TREAD_3':
+				return 1.1;
+			case 'TREAD_4':
+				return 0.9;
+			default: throw new Error('UNEXPECTED TREAD TYPE: '+this.treadType);
+		}
+	}
+
+	getTurnSpeedMultiplier(): number {
+		switch (this.treadType) {
+			case 'TREAD_1':
+				return 0.9;
+			case 'TREAD_2':
+				return 1.1;
+			case 'TREAD_3':
+				return 0.95;
+			case 'TREAD_4':
+				return 1.05;
+			default: throw new Error('UNEXPECTED TREAD TYPE: '+this.treadType);
+		}
+	}
+
+	getArmorOffset(): number {
+		switch (this.treadType) {
+			case 'TREAD_1':
+				return 50;
+			case 'TREAD_2':
+				return 0;
+			case 'TREAD_3':
+				return 0;
+			case 'TREAD_4':
+				return 25;
+			default: throw new Error('UNEXPECTED TREAD TYPE: '+this.treadType);
+		}
+	}
+
+	getPointCost(): number {
+		switch (this.treadType) {
+			case 'TREAD_1':
+				return 2;
+			case 'TREAD_2':
+				return 2;
+			case 'TREAD_3':
+				return 1;
+			case 'TREAD_4':
+				return 1;
+			default: throw new Error('UNEXPECTED TREAD TYPE: '+this.treadType);
+		}
 	}
 
 }
