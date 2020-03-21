6abe589d311ffdc60b5294e5f814b510c29f05c0
Emil Dolorfino <edolorfino24@gmail.com>
2020-03-20 20:36:30 -0400

    replayController: Updated Req and Res imports

--- src/backend/controllers/replayController.js
+++ src/backend/controllers/replayController.js
@@ -2,13 +2,13 @@
 
 // Required imports
 import { validationResult } from 'express-validator';
-import type { $Request, $Response } from 'express';
+import type { Request, Response } from 'express';
 
 // Model imports
 import User from '../../models/userModel';
 import BattleRecord from '../../models/battleRecordModel';
 
-exports.getReplayList = async (req: $Request, res: $Response) => {
+exports.getReplayList = async (req: Request, res: Response) => {
     // Gather any errors from the route checking
     const errors = validationResult(req);
 
