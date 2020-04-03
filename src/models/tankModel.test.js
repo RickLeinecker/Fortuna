dfbaa77822f8a29ebec0e0ff4dbc61737b8102d3
Emil Dolorfino <edolorfino24@gmail.com>
2020-04-03 16:59:16 -0400

    init on tankModel tests

---
+++ src/models/tankModel.test.js
@@ -0,0 +1,41 @@
+// Testing the Token Model from the data layer
+
+// Required imports
+const mongoose = require("mongoose");
+
+// User Model
+const Tank = require('./tankModel');
+
+// Create test database
+const testDB = "mongodb://localhost/test";
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+};
+mongoose.connect(testDB, options)
+    .then(() => console.log("Connected to Test Database"))
+    .catch(() => console.error("Could not connect to Test Database"));
+
+describe("Tank model tests", () => {
+    // Run before all tests are tried
+    beforeAll(async () => {
+        await Tank.deleteMany({});
+    });
+
+    // Run after each test
+    afterEach(async () => {
+        await Tank.deleteMany({});
+    });
+
+    // Run after all tests are done
+    afterAll(async () => {
+        await mongoose.connection.close();
+    });
+
+    // Tests if the Tanks have a module
+    it("has a module", () => {
+        expect(Tank).toBeDefined();
+    });
+});
