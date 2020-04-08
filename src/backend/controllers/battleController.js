c5d69ea3cab8c43d8538354ae6c092baf73d8f96
Emil Dolorfino <edolorfino24@gmail.com>
2020-04-08 03:11:04 -0400

    added logic for first win of day and to deal with the first win of day streaks.

--- src/backend/controllers/battleController.js
+++ src/backend/controllers/battleController.js
@@ -34,7 +34,7 @@ exports.prepareMatch = async (req: Request, res: Response) => {
 				.status(404)
 				.json({ msg: 'Could not find the personBeingChallenged in DB'})
 		}
-		else if (personBeingChallengedUserDoc.wager == 0) { // Check that they have a wager set
+		else if (personBeingChallengedUserDoc.wager === 0) { // Check that they have a wager set
 			console.log('personBeingChallenged does not have a wager set')
 			return res
 				.status(400)
@@ -57,7 +57,7 @@ exports.prepareMatch = async (req: Request, res: Response) => {
 				.json({ msg: "Could not find the challenger's Tank in DB"});
 		}
 
-		if (challengerTank.userId == personBeingChallengedId) {
+		if (challengerTank.userId === personBeingChallengedId) {
 			console.log('Cannot challenge self');
 			return res
 				.status(400)
@@ -150,8 +150,8 @@ exports.reportResults = async (req: Request, res: Response) => {
 				.status(404)
 				.json({ msg: 'Could not find battle'});
 		}
-		else if (battle.winner != -1) { // Check if battle has already been reported
-			if (battle.winner == winner) {
+		else if (battle.winner !== -1) { // Check if battle has already been reported
+			if (battle.winner === winner) {
 				console.log('Match already reported');
 				return res
 					.status(200)
@@ -210,7 +210,7 @@ exports.reportResults = async (req: Request, res: Response) => {
 		else if (winner === 1) { // userOne victory
 
 			// Update money and wins for user one
-			const userOne = await User.findByIdAndUpdate(battle.userOne, { $inc: { money : battle.prizeMoney, 'stats.wins' : 1 } }, {new: true});
+			const userOne = await User.findById(battle.userOne);
 
 			if (userOne == null) {
 				console.log('userOne not found')
@@ -218,6 +218,39 @@ exports.reportResults = async (req: Request, res: Response) => {
 					.status(404)
 					.json({ msg: 'Could not find userOne'});
 			}
+
+			// Update money with first win of day bonus, if applicable
+			const aDay = 60 * 60 * 24 * 1000;
+			const compare = (!userOne.stats.lastFirstWinOfDay) ? (new Date() - userOne.stats.lastFirstWinOfDay) : 0;
+			// If last recorded first win of the day is null or it's been over a day since
+			if (userOne.stats.lastFirstWinOfDay == null || compare > aDay) {
+				// If it's been over 2 days since, the streak is broken
+				if (compare > (aDay*2)) {
+					userOne.stats.firstWinOfDayStreak = 0;
+					console.log("Streak Broken :(");	
+				}
+				// Give bonus based on current streak
+				if (userOne.stats.firstWinOfDayStreak === 0) {
+					// +100 for first day of the streak
+					userOne.money += (battle.prizeMoney + 100);
+					userOne.stats.firstWinOfDayStreak++;
+					console.log("Given First Win of Day 1 Bonus");
+				}
+				else if (userOne.stats.firstWinOfDayStreak === 1) {
+					// +200 for second day of the streak
+					userOne.money += (battle.prizeMoney + 200);
+					userOne.stats.firstWinOfDayStreak++;
+					console.log("Given First Win of Day 2 Bonus");					
+				}
+				else if (userOne.stats.firstWinOfDayStreak === 2) {
+					// +300 for third day of the streak, and streak resets
+					userOne.money += (battle.prizeMoney + 300);
+					userOne.stats.firstWinOfDayStreak = 0;
+					console.log("Given First Win of Day 3 Bonus. Resetting streak.");					
+				}
+			}
+			// Update stats
+			userOne.stats.wins += 1;
 
 			// Update money and losses for user two
 			const userTwo = await User.findByIdAndUpdate(battle.userTwo, { $inc: { 'stats.losses' : 1 } }, {new: true});
@@ -258,7 +291,7 @@ exports.reportResults = async (req: Request, res: Response) => {
 			}
 
 			// Update money and wins for user two
-			const userTwo = await User.findByIdAndUpdate(battle.userTwo, { $inc: { money : battle.prizeMoney, 'stats.wins' : 1 } }, {new: true});
+			const userTwo = await User.findById(battle.userTwo);
 
 			if (userTwo == null) {
 				console.log('userTwo not found')
@@ -266,6 +299,39 @@ exports.reportResults = async (req: Request, res: Response) => {
 					.status(404)
 					.json({ msg: 'Could not find userTwo'});
 			}
+
+			// Update money with first win of day bonus, if applicable
+			const aDay = 60 * 60 * 24 * 1000;
+			const compare = (!userTwo.stats.lastFirstWinOfDay) ? (new Date() - userTwo.stats.lastFirstWinOfDay) : 0;
+			// If last recorded first win of the day is null or it's been over a day since
+			if (userTwo.stats.lastFirstWinOfDay == null || compare > aDay) {
+				// If it's been over 2 days since, the streak is broken
+				if (compare > (aDay*2)) {
+					userTwo.stats.firstWinOfDayStreak = 0;
+					console.log("Streak Broken :(");	
+				}
+				// Give bonus based on current streak
+				if (userTwo.stats.firstWinOfDayStreak === 0) {
+					// +100 for first day of the streak
+					userTwo.money += (battle.prizeMoney + 100);
+					userTwo.stats.firstWinOfDayStreak++;
+					console.log("Given First Win of Day 1 Bonus");
+				}
+				else if (userTwo.stats.firstWinOfDayStreak === 1) {
+					// +200 for second day of the streak
+					userTwo.money += (battle.prizeMoney + 200);
+					userTwo.stats.firstWinOfDayStreak++;
+					console.log("Given First Win of Day 2 Bonus");					
+				}
+				else if (userTwo.stats.firstWinOfDayStreak === 2) {
+					// +300 for third day of the streak, and streak resets
+					userTwo.money += (battle.prizeMoney + 300);
+					userTwo.stats.firstWinOfDayStreak = 0;
+					console.log("Given First Win of Day 3 Bonus. Resetting streak.");					
+				}
+			}
+			// Update stats
+			userTwo.stats.wins += 1;
 
 			// Calculate new elo values, returns the value of the player in the first parameter
 			const newUserOneElo = eloRating.ifLoses(userOne.stats.elo, userTwo.stats.elo);
