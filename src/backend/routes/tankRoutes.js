96e9779bb5f3cb5df7fd290fb44bb36536330742
Emil Dolorfino <edolorfino24@gmail.com>
2020-04-14 19:28:42 -0400

    edited backend to handle a JSON stringified Array of mongoIds

--- src/backend/routes/tankRoutes.js
+++ src/backend/routes/tankRoutes.js
@@ -118,7 +118,7 @@ router.get('/getBotTanks', tankController.getBotTanks);
 // Takes an array of tank ids
 // returns the tank objects
 router.get('/getTanksById', [
-	check('array', 'An array of MongoIds is required.').isArray().isMongoId()
+	check('array', 'A JSON array of MongoIds is required.').isJSON()
 ], tankController.getTankById);
 
 module.exports = router;
