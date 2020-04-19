6796836466d623bc67bbce77e126d83719a83ef6
David Harmeyer <13dharmeyer@gmail.com>
2020-04-19 12:45:24 -0400

    Fixed nitro repair

--- src/tanks/Tank.js
+++ src/tanks/Tank.js
@@ -318,7 +318,7 @@ class Tank extends GameObject {
 		//nitro repair stuff
 		if (this.haveNitroRepair && this._getBoolean(USE_NITRO_REPAIR_VAR_NAME)) {
 			this.haveNitroRepair=false;
-			this.health=Math.min(NITRO_REPAIR_HEALTH_BONUS, this._getArmorOffset());
+			this.health=Math.min(this.health+NITRO_REPAIR_HEALTH_BONUS, this._getArmorOffset());
 			this.nitroRepairTimerLeft=NITRO_REPAIR_LENGTH;
 		}
 		this.nitroRepairTimerLeft--;
