cf337a071e03ccd5a8fe2da1d73c8a834bdd4133
Brittanie <bs009@live.com>
2021-01-16 14:58:49 -0500

    main menu button animations

--- frontend_fortuna_v2/src/mainmenu/MainMenu.js
+++ frontend_fortuna_v2/src/mainmenu/MainMenu.js
@@ -1,77 +1,87 @@
 //@flow strict
 
 import './MainMenu.css';
-import * as React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import MainNavbar from '../globalComponents/MainNavbar.js';
 import { verifyLink } from '../globalComponents/verifyLink.js';
 import { verifyLogin } from '../globalComponents/apiCalls/verifyLogin.js';
 import { Transition } from 'react-transition-group'
-import { TweenMax, TweenLite } from 'gsap/all'
+import { TweenMax, TweenLite, Power3 } from 'gsap'
 
 // MAY NEED TO CHAGNE THIS TO A FUNCTIONAL COMPONENT
 // THIS HAS NO STATE
 
-// Main Menu component.
-class MainMenu extends React.Component<{||}> {
+function MainMenu() {
 
-	constructor(props) {
-		super(props);
-    verifyLogin();
-  }
-  
-  componentDidMount() {
-    document.body.style.backgroundImage = "url('/login_background.gif')"
-    document.body.style.fontFamily = "font-family: 'Press Start 2P', cursive;"
-  }
+  let mp = useRef(null);
+  let ba = useRef(null);
+  let ta = useRef(null);
+  let arm = useRef(null);
 
-	render(): React.Node {
-    const { show } = this.props;
-    const startState = { autoAlpha: 0, y: -50 }
-		return (
-			<div id="Parent">
-				<MainNavbar 
-					linkName="/Login" 
-					returnName="Logout" 
-					pageName="Main Menu" 
-				/>
-				<h1 className="menuheader">Where to Commander?</h1>
-        <Transition
-          unmountOnExit
-          in={show}
-          timeout={1000}
-          onEnter={node => TweenLite.set(node, startState)}
-          addEndListener={ (node, done) => {
-            TweenLite.to(node, 0.5, {
-              autoAlpha: show ? 1 : 0,
-              y: show ? 0 : 50,
-              onComplete: done
-            });
-          }}
-        >
-          <div className="column menuleft">
-            <Link to={verifyLink("/Marketplace")}>
-              <button className="mainMenuBtn">Marketplace</button>
-            </Link>
-          </div>
-        </Transition>
-				<div className="column menumiddle">
-					<Link to={verifyLink("/BattleArena")}>
-						<button className="mainMenuBtn">Battle Arena</button>
-					</Link>
-					<br/><br/><br/>
-					<Link to={verifyLink("/TrainingArena")}>
-						<button className="mainMenuBtn">Training Arena</button>
-					</Link>
-				</div>
-				<div className="column menuright">
-					<Link to={verifyLink("/Armory")}>
-						<button className="mainMenuBtn">Armory</button>
-					</Link>
-				</div>
-			</div>
-		);
-	}
+
+  useEffect(() => {
+    TweenMax.to(
+      mp,
+      0.8,
+      {
+        opacity: 1,
+        y: -70,
+        ease: Power3.easeOut
+      }
+    )
+
+    TweenMax.to(
+      ba,
+      0.8,
+      {
+        opacity: 1,
+        y: -70,
+        ease: Power3.easeOut
+      }
+    )
+
+    TweenMax.to(
+      arm,
+      0.8,
+      {
+        opacity: 1,
+        y: -70,
+        ease: Power3.easeOut
+      }
+    )
+    
+  }, [])
+
+  return (
+    <div id="Parent">
+      <MainNavbar 
+        linkName="/Login" 
+        returnName="Logout" 
+        pageName="Main Menu" 
+      />
+      <h1 className="menuheader">Where to Commander?</h1>
+      <div ref={mpref => {mp = mpref}} className="column menuleft">
+        <Link to={verifyLink("/Marketplace")}>
+          <button className="mainMenuBtn">Marketplace</button>
+        </Link>
+      </div>
+      <div ref={baref => {ba = baref}} className="column menumiddle">
+        <Link to={verifyLink("/BattleArena")}>
+          <button className="mainMenuBtn">Battle Arena</button>
+        </Link>
+        <br/><br/><br/>
+        <Link to={verifyLink("/TrainingArena")}>
+          <button className="mainMenuBtn">Training Arena</button>
+        </Link>
+      </div>
+      <div ref={armref => {arm = armref}} className="column menuright">
+        <Link to={verifyLink("/Armory")}>
+          <button className="mainMenuBtn">Armory</button>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
-export default MainMenu;
+export default MainMenu
