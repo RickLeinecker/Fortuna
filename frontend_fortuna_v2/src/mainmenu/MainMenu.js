8d24b5065172963e35240c83f5f3004faaeb9d5a
Brittanie <bs009@live.com>
2021-01-25 17:31:36 -0500

    added joyride skeleton to main menu

--- frontend_fortuna_v2/src/mainmenu/MainMenu.js
+++ frontend_fortuna_v2/src/mainmenu/MainMenu.js
@@ -6,13 +6,37 @@ import { Link } from 'react-router-dom';
 import MainNavbar from '../globalComponents/MainNavbar.js';
 import { verifyLink } from '../globalComponents/verifyLink.js';
 import { verifyLogin } from '../globalComponents/apiCalls/verifyLogin.js';
+import JoyRide from 'react-joyride';
 
 // Main Menu component.
 class MainMenu extends React.Component<{||}> {
 
 	constructor() {
 		super();
-		verifyLogin();
+    verifyLogin();
+
+    this.state = {
+      TOUR_STEPS: [
+        {
+          target: ".mainMenuBtn",
+          content: "This is where you can search the dashboard."
+        },
+        {
+          target: ".btlarena",
+          content:
+            "Bar chart for total order. You can see beautiful graphs here, thanks to creative tim for such UI."
+        },
+        {
+          target: ".trainarena",
+          content: "This is where you can find the external links."
+        },
+        {
+          target: ".armory",
+          content: "This is where you can see the footer links."
+        }
+      ]
+    }
+    
   }
   
   componentDidMount() {
@@ -22,33 +46,51 @@ class MainMenu extends React.Component<{||}> {
 
 	render(): React.Node {
 		return (
-			<div id="Parent">
-				<MainNavbar 
-					linkName="/Login" 
-					returnName="Logout" 
-					pageName="Main Menu" 
-				/>
-				<h1 className="menuheader">Where to Commander?</h1>
- 				<div className="column menuleft">
-				 	<Link to={verifyLink("/Marketplace")}>
-						<button className="mainMenuBtn">Marketplace</button>
-					</Link>
-				</div>
-				<div className="column menumiddle">
-					<Link to={verifyLink("/BattleArena")}>
-						<button className="mainMenuBtn">Battle Arena</button>
-					</Link>
-					<br/><br/><br/>
-					<Link to={verifyLink("/TrainingArena")}>
-						<button className="mainMenuBtn">Training Arena</button>
-					</Link>
-				</div>
-				<div className="column menuright">
-					<Link to={verifyLink("/Armory")}>
-						<button className="mainMenuBtn">Armory</button>
-					</Link>
-				</div>
-			</div>
+      <>
+        <div id="Parent">
+          <MainNavbar 
+            linkName="/Login" 
+            returnName="Logout" 
+            pageName="Main Menu" 
+          />
+          <h1 className="menuheader">Where to Commander?</h1>
+          <div className="column menuleft">
+            <Link to={verifyLink("/Marketplace")}>
+              <button className="mainMenuBtn">Marketplace</button>
+            </Link>
+          </div>
+          <div className="column menumiddle">
+            <Link to={verifyLink("/BattleArena")}>
+              <button className="mainMenuBtn btlarena">Battle Arena</button>
+            </Link>
+            <br/><br/><br/>
+            <Link to={verifyLink("/TrainingArena")}>
+              <button className="mainMenuBtn trainarena">Training Arena</button>
+            </Link>
+          </div>
+          <div className="column menuright">
+            <Link to={verifyLink("/Armory")}>
+              <button className="mainMenuBtn armory">Armory</button>
+            </Link>
+          </div>
+        </div>
+        <JoyRide 
+              steps={this.state.TOUR_STEPS} 
+              continuous={true} 
+              styles={{
+                options: {
+                  arrowColor: '#e3ffeb',
+                  backgroundColor: 'rgb(245, 59, 229)',
+                  overlayColor: 'rgba(79, 26, 0, 0.4)',
+                  primaryColor: '#000',
+                  textColor: 'white',
+                  width: 200,
+                  zIndex: 1000,
+                }
+              }}
+          />
+      </>
+
 		);
 	}
 }
