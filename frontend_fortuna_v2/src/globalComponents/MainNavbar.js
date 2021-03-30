3869a6a3db1e72a0d68a704ad6180489f33e3978
Brittanie <bs009@live.com>
2021-03-30 16:03:47 -0400

    navbar tweak

--- frontend_fortuna_v2/src/globalComponents/MainNavbar.js
+++ frontend_fortuna_v2/src/globalComponents/MainNavbar.js
@@ -102,7 +102,7 @@ class MainNavbar extends React.Component<Props, State> {
 
 		const link = (this.props.linkName==null || this.props.returnName==null) ? null : (
 			<Link to={verifyLink(this.props.linkName)}>
-        <br/><br/>
+        <br/>
 				<button onClick={this.handleLogout.bind(this)} className="navbtn">&#60;&#45; {this.props.returnName}</button>
 			</Link>
 		);
