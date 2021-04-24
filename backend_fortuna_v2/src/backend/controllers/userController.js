435709408d511f6e7d4a6d9b93878057ceb74e46
Will <wbecht1@knights.ucf.edu>
2021-04-24 16:33:52 -0400

    Changed default dan to Kiona's Tank

--- backend_fortuna_v2/src/backend/controllers/userController.js
+++ backend_fortuna_v2/src/backend/controllers/userController.js
@@ -94,7 +94,7 @@ exports.googleLogin = async (req, response) => {
             // Create initial tanks for user
             let tank1 = new Tank();
             tank1.userId = user.id;
-            tank1.tankName = `Default Dan`;
+            tank1.tankName = `Kiona's Tank`;
             tank1.components = ['moddable', 'machineGun', 'empty', 'empty', 'empty', 'empty', 'empty', 'advancedTreads', 'empty', 'empty', 'empty'];
             tank1.casusCode = {
               boundingBox: { x: 0, y: 0, w: 64, h: 23 },
