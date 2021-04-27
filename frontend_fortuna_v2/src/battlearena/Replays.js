d881a99111c9de2243020c8513de217bc0914800
Will <wbecht1@knights.ucf.edu>
2021-04-26 20:40:37 -0400

    Fixed Battle Replay Title sizing

--- frontend_fortuna_v2/src/battlearena/Replays.js
+++ frontend_fortuna_v2/src/battlearena/Replays.js
@@ -67,6 +67,8 @@ class Replays extends React.Component<Props, State> {
 		backgroundColor: "rgba(0,0,0,.5)",
 		fontFamily: '"Press Start 2P", cursive',
 		color:"white",
+		minWidth: '455px',
+		maxWidth: '80%',
 		fontSize: 16,
     padding: "5px"
 	}
